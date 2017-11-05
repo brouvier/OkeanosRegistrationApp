51b78f60cd729c176f252110d3dcca3db9d9c1fe
brouvier <brouvier@gmail.com>
2017-11-05 23:06:51 +0100

    Génération de classeur d'équipe - Backend

--- src/main/java/okeanos/dao/AdherentInfoSaisonDao.java
+++ src/main/java/okeanos/dao/AdherentInfoSaisonDao.java
@@ -26,14 +26,25 @@ public class AdherentInfoSaisonDao {
 	}
 
 	public static List<AdherentInfoSaison> getAllItemsForSaison(Long saisonId) {
+		return getAllItemsForSaison(saisonId, null);
+	}
+
+	public static List<AdherentInfoSaison> getAllItemsForSaison(Long saisonId, Long teamId) {
+		String teamClause = "";
+		if (teamId != null) {
+			teamClause = " AND fk_team_id = :teamId ";
+		}
+
 		String sql = "SELECT id, fk_account_id, fk_saison_id, fk_ffessm_licence_id, fk_subscription_id, "
 				+ "fk_insurance_id, picture_authorisation, nead_certificate, fk_actual_training_id, fk_training_id, "
 				+ "fk_team_id, fk_sick_note_id, fk_parental_agreement_id, fk_certificate_licence_id, validation_start, validation_general_informations, "
 				+ "validation_licence, validation_sick_note, validation_parental_agreement, validation_payment_transmitted, "
-				+ "validation_payment_cashed, validation_comment FROM adherent_info_saison WHERE fk_saison_id = :saisonId ORDER BY id";
+				+ "validation_payment_cashed, validation_comment FROM adherent_info_saison "
+				+ "WHERE fk_saison_id = :saisonId " + teamClause + "ORDER BY id";
 
 		try (Connection con = Sql2oDao.sql2o.open()) {
-			return con.createQuery(sql).addParameter("saisonId", saisonId).executeAndFetch(AdherentInfoSaison.class);
+			return con.createQuery(sql).addParameter("saisonId", saisonId).addParameter("teamId", teamId)
+					.executeAndFetch(AdherentInfoSaison.class);
 		}
 	}
 
--- src/main/java/okeanos/restApp/resources/HockeyTeamResource.java
+++ src/main/java/okeanos/restApp/resources/HockeyTeamResource.java
@@ -4,16 +4,40 @@ import static spark.Spark.delete;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
-import com.google.gson.Gson;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.jetty.io.EofException;
+
+import com.google.gson.Gson;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.utils.PdfMerger;
+
+import okeanos.dao.AdherentDocumentDao;
+import okeanos.dao.AdherentInfoSaisonDao;
 import okeanos.dao.HockeyTeamDao;
+import okeanos.dao.SaisonDao;
+import okeanos.model.AdherentDocument;
+import okeanos.model.AdherentInfoSaison;
 import okeanos.model.HockeyTeam;
+import okeanos.model.Saison;
 import okeanos.util.AppProperties;
+import spark.Response;
 
 public class HockeyTeamResource extends AbstractResource {
 
 	protected String ressourcePath = AppProperties.API_CONTEXT + "/hockey_team";
 
+	@SuppressWarnings("deprecation")
 	public HockeyTeamResource() {
 		super.setupEndpoints();
 
@@ -48,6 +72,91 @@ public class HockeyTeamResource extends AbstractResource {
 			}
 			return gson.toJson(HockeyTeamDao.save(new Gson().fromJson(request.body(), HockeyTeam.class)));
 		});
+
+		get(ressourcePath + "/binder_team/:teamId/saison/:saisonId", (request, response) -> {
+			setSecurity(request, response);
+
+			if (!SecurityResource.isAdmin(request)) {
+				throw new IllegalAccessException("Illegal Access");
+			}
+
+			HockeyTeam team = HockeyTeamDao.getItemById(new Long(request.params(":teamId")));
+			Saison saison = SaisonDao.getItemById(new Long(request.params(":saisonId")));
+
+			if (team.getId() == null || saison.getId() == null) {
+				throw new IllegalStateException("Missing Parameter");
+			}
+
+			return sendPDFFile(response, mergePDFFile(team.getId(), saison.getId()));
+		});
+	}
+
+	/**
+	 * Construction du classeur d'une equipe en mergeant l'ensemble des documents
+	 * des membres de l'équipe
+	 * 
+	 * @param teamId
+	 *            Identifiant de l'équipe dont les documents doivent être extraient
+	 * @return Le PDF mergé sous la forme d'un tableau de byte
+	 */
+	protected byte[] mergePDFFile(Long teamId, Long saisonId) throws IOException, IllegalStateException {
+		byte[] bytes = null;
+
+		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();) {
+			PdfDocument pdfDoc = new PdfDocument(new PdfWriter(outputStream));
+			PdfMerger merger = new PdfMerger(pdfDoc);
+
+			List<AdherentInfoSaison> teamMember = AdherentInfoSaisonDao.getAllItemsForSaison(saisonId, teamId);
+			logger.warn("Nb adherents : {}", teamMember.size());
+
+			if (teamMember == null || teamMember.size() == 0) {
+				throw new IllegalStateException("Missing Parameter");
+			}
+
+			for (AdherentInfoSaison item : AdherentInfoSaisonDao.getAllItems()) {
+				// logger.warn("Adherent id {}, Doc id {}", item.getId(),
+				// item.getFk_sick_note_id());
+				if (item.getFk_sick_note_id() != null) {
+					AdherentDocument doc = AdherentDocumentDao.getItemById(item.getFk_sick_note_id());
+					// logger.warn("Doc {}", doc);
+
+					if ("application/pdf".equals(doc.getFile_type())) {
+						try (InputStream inputStream = new ByteArrayInputStream(doc.getData());) {
+							PdfDocument srcDoc1 = new PdfDocument(new PdfReader(inputStream));
+							merger.merge(srcDoc1, 1, srcDoc1.getNumberOfPages());
+							srcDoc1.close();
+						}
+
+					}
+				}
+			}
+
+			// Récupération du pdf mergé
+			pdfDoc.close();
+			bytes = outputStream.toByteArray();
+		}
+		return bytes;
+	}
+
+	/**
+	 * Formatage d'un PDF pour l'envoyer dans la réponse du serveur
+	 * 
+	 * @param bytes
+	 *            Le fichier PDF sous forme de tableau de Byte
+	 */
+	protected HttpServletResponse sendPDFFile(Response response, byte[] bytes) throws IOException {
+		response.type("application/pdf");
+		response.header("Content-Disposition", "filename=\"ClasseurEquipe.pdf\"");
+		response.raw().setContentLengthLong(bytes.length);
+
+		// Construction du flux de réponse
+		try (ServletOutputStream out = response.raw().getOutputStream();
+				ByteArrayInputStream in = new ByteArrayInputStream(bytes);) {
+			IOUtils.copyLarge(in, out);
+		} catch (EofException eofe) { // no problem
+		}
+
+		return response.raw();
 	}
 
 }
